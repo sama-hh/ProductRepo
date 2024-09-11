fcc503f75afbea34814e4943f7ed1ef70fd2a41b
sama-hh <samahajimuradovah@gmail.com>
2024-09-11 15:21:14 +0200

    add scanner and colors

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,16 @@ package org.example;
 
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String GREEN = "\033[32m";
+        String RESET = "\033[0m";
+        String CYAN = "\033[36m";
+
         Product product1 = new Product("1", "Apple", 10);
         Product product2 = new Product("2", "Banana",7);
         Product product3 = new Product("3", "Grape", 12);
@@ -23,5 +30,15 @@ public class Main {
 
         ShopService shopService = new ShopService(orderListRepo, productRepo);
         shopService.placeOrder("1" , Arrays.asList(orderedProduct1, orderedProduct2));
+
+        System.out.println(GREEN + "Starting order" + RESET);
+        System.out.print(CYAN + "Enter product id: " + RESET );
+        String productId = scanner.nextLine();
+        System.out.print(CYAN + "Enter quantity: " + RESET);
+        int quantity = scanner.nextInt();
+
+        OrderProduct orderedProduct3 = new OrderProduct(productId, quantity);
+        shopService.placeOrder("2", List.of(orderedProduct3));
+
     }
 }
